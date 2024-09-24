b186305b804c92feae447704fd4c18b25cbb7c43
JoshuaYeoh1 <joshuayeohkeatkeong@gmail.com>
2024-09-24 08:22:22 +0800

    Begun planning for Anaya State Machine

---
+++ Anaya The Great/Assets/Scripts/Yeoh/Anaya/AnayaStateMachine.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnayaStateMachine : MonoBehaviour
+{
+    public Anaya anaya;
+
+    // sub state machines
+    public GameObject Pilot_SM;
+    public GameObject AI_SM;
+    public GameObject Action_SM;
+
+    StateMachine sm;
+    BaseState defaultState;
+
+    void Start()
+    {
+        sm = new StateMachine();
+        
+        // STATES ================================================================================
+
+        State_Hub hub = new();
+        //AnayaState_Grounded grounded = new(this);
+        //AnayaState_MidAir midAir = new(this);
+
+        //temp
+        AnayaState_Idle idle = new(this);
+
+        // TRANSITIONS ================================================================================
+
+        hub.AddTransition(idle, (timeInState) =>
+        {
+            // if(
+            //     &&
+            // ){
+            //     return true;
+            // }
+            return true;
+        });
+        
+        // ================================================================================
+
+        // idle.AddTransition(grounded, (timeInState) =>
+        // {
+        //     // if(
+        //     //     ||
+        //     // ){
+        //     //     return true;
+        //     // }
+        //     return false;
+        // });
+
+        // DEFAULT ================================================================================
+        
+        defaultState = hub;
+        sm.SetInitialState(defaultState);
+    }
+
+    void Update()
+    {
+        sm.Tick(Time.deltaTime);
+    }
+
+    void OnDisable()
+    {
+        if(sm!=null)
+        {
+            sm.currentState.Exit(); // call OnExit on current state
+            sm.SetState(defaultState); // Change back to default state
+        }
+    }
+}
